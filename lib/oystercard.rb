5fd30f30e266e924e3449c6514ed010b5540651c
Francesco <francesco@monzo.com>
2022-02-01 17:04:27 +0100

    Added test for deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,14 +2,14 @@ class Oystercard
 
     attr_reader :balance
     LIMIT = 90
-  
+
     def initialize
       @balance = 0
     end
-  
+
     def top_up(value)
-      fail "£90 limit exceeded" if balance + value > LIMIT
+      fail "£#{LIMIT} limit exceeded" if balance + value > LIMIT
       @balance += value
     end
-  
+
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do
-  
+
   it "has a balance of 0 upon initialization" do
     expect(subject.balance).to eq 0
   end
-  describe "#top_up" do 
-    
+  describe "#top_up" do
+
     it { is_expected.to respond_to(:top_up).with(1).argument }
-  
+
     it "tops up the balance with the argument balance provided" do
       subject.top_up(5)
       expect(subject.balance).to eq 5
@@ -17,8 +17,15 @@ describe Oystercard do
     it "should fail if balance is more than £90" do
       limit = Oystercard::LIMIT
       subject.top_up(limit)
-      expect { subject.top_up(1)}.to raise_error "£90 limit exceeded"
+      expect { subject.top_up(1)}.to raise_error "£#{limit} limit exceeded"
     end
-
+  end
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct) }
+    it "deducts the balance by value amount" do
+      card = subject.top_up(25)
+      card.deduct(10)
+      expect(card.balance).to eq 15
+    end
   end
 end
