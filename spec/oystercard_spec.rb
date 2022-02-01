709cca8693d43ee06a1a8b246aa189af53513f82
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-02-01 15:36:49 +0000

    Chapter 6 maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,8 +10,15 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
   
     it "tops up the balance with the argument balance provided" do
-      subject.top_up(500)
-      expect(subject.balance).to eq 500
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
     end
+
+    it "should fail if balance is more than £90" do
+      limit = Oystercard::LIMIT
+      subject.top_up(limit)
+      expect { subject.top_up(1)}.to raise_error "£90 limit exceeded"
+    end
+
   end
 end
